dc93d7b09ce8678eb16718ec5b611dfcd5593295
John Jorgensen <jjorgensen@hsdp.io>
2026-05-04 08:12:49 -0400

    Remove old loop scripts

--- loop-grow.js
+++
@@ -1,7 +0,0 @@
-export async function main(ns) {
-    // args:
-    // host to grow (string)
-    while (true) {
-        ns.grow(ns.args[0]);
-    }
-}
--- loop-weaken.js
+++
@@ -1,7 +0,0 @@
-export async function main(ns) {
-    // args:
-    // host to weaken (string)
-    while (true) {
-        ns.weaken(ns.args[0]);
-    }
-}
